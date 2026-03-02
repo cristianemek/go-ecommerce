fced9dd86d0c42ec46c254335a8cb035711f637d
Cristianemek <cristianrodriguezlugo@gmail.com>
2026-03-03 00:01:01 +0100

    Agregar función GetString para manejar variables de entorno y actualizar la configuración de la base de datos en main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,22 +1,41 @@
 package main
 
 import (
+	"context"
 	"log/slog"
 	"os"
+
+	"github.com/cristianemek/go-ecommerce/internal/env"
+	"github.com/jackc/pgx/v5"
 )
 
 func main() {
+	ctx := context.Background()
+
 	cfg := config{
 		addr: ":8080",
-		db:   dbConfig{},
+		db: dbConfig{
+			dsn: env.GetString("GOOSE_DBSTRING", "user=postgres password=postgres dbname=ecom sslmode=disable host=localhost"),
+		},
 	}
+
+	//logger
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+
+	//database
+	conn, err := pgx.Connect(ctx, cfg.db.dsn)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close(ctx)
+
+	logger.Info("connected to database", "dsn", cfg.db.dsn)
 
 	api := application{
 		config: cfg,
+		db:     conn,
 	}
-
-	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
-	slog.SetDefault(logger)
 
 	if err := api.run(api.mount()); err != nil {
 		slog.Error("failed to start server", "error", err)
