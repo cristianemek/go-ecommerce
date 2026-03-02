cc0b7062f709a46f0781097448b2678f8903f23e
Cristianemek <cristianrodriguezlugo@gmail.com>
2026-03-03 00:01:19 +0100

    Refactor servicio de productos: actualizar ListProducts para devolver productos y ajustar el handler correspondiente

--- internal/products/handlers.go
+++ internal/products/handlers.go
@@ -4,7 +4,6 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/cristianemek/go-ecommerce/internal/products"
 	"github.com/cristianemek/go-ecommerce/internal/products/json"
 )
 
@@ -20,14 +19,12 @@ func NewHandler(service Service) *handle {
 
 // w http.ResponseWriter, es la response que se va a enviar al cliente, y r *http.Request es la solicitud que se recibe del cliente (donde viene body, query params, etc)
 func (h *handle) ListProducts(w http.ResponseWriter, r *http.Request) {
-	err := h.service.ListProducts(r.Context())
+	products, err := h.service.ListProducts(r.Context())
 
 	if err != nil {
 		log.Println("error listing products:", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	products := []string{"product1", "product2", "product3"}
-
 	json.Write(w, http.StatusOK, products)
 }
